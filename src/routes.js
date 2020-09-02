5c6244df0d13b5e09b35fb71fa992b4c5e55d4c1
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2020-09-02 10:10:57 -0300

    middleware de authenticação

--- src/routes.js
+++ src/routes.js
@@ -6,24 +6,16 @@ import SessionControllerPsychologist from './app/controllers/SessionControllerPs
 
 import Psychologist from './app/models/Psychologist';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/sessionpatient', SessionControllerPatient.store);
 routes.post('/sessionpsychologist', SessionControllerPsychologist.store);
+
+routes.use(authMiddleware);
+
 routes.post('/patients', PatientController.store);
 routes.get('/patients', PatientController.index);
-
-routes.get('/', async (request, response) => {
-  await Psychologist.create({
-    psy_name: 'rodrigokakjs',
-    psy_email: 'rodrigo@123teste.com',
-    psy_password: '123456',
-    psy_cpf: '097998734',
-    psy_crp: '345345345',
-    psy_data_nasc: '2020-08-31T13',
-    psy_city: '12wefrsdfs',
-    psy_availability: ['2020-08-29T20:50:51.853Z'],
-  })
-})
 
 export default routes;
