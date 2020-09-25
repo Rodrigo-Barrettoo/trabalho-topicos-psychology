73b1499bc720b906b09a81dfa4dc52ed3e195338
Carlos Xavier <carlosaugustoxpaiva@gmail.com>
2020-09-25 15:31:11 -0300

    Correção de conflitos

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import routes from "./routes";
-import cors from 'cors';
+import cors from "cors";
 var path = require("path");
 
 import "./database";
@@ -15,14 +15,7 @@ class App {
 
   middlewares() {
     this.server.use(express.json());
-<<<<<<< HEAD
-    this.server.use(function (req, res, next) {
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      next();
-    });
-=======
     this.server.use(cors());
->>>>>>> 24271f530d2f24d9806d5a4e722e3bb3727de8f9
     this.server.set("view engine", "ejs");
     this.server.set("views", path.join(__dirname, "views"));
   }
